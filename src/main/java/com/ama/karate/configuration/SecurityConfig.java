cb2aa2d1eaa3d87a290dfd43dcb6df80e2da4bdd
AkashFestivs <gadhaveakash999@gmail.com>
2024-10-23 01:20:41 +0530

    commit security

--- src/main/java/com/ama/karate/configuration/SecurityConfig.java
+++ src/main/java/com/ama/karate/configuration/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.ama.karate.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Configuration
 @EnableWebSecurity
@@ -20,15 +24,21 @@ public class SecurityConfig {
         this.customAuthProvider = customAuthProvider;
     }
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        System.out.println("security called:::::::::::::::::::::::::");
         http
             .csrf(csrf -> csrf.disable())
             .authenticationProvider(customAuthProvider)
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/authenticate").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().access((authentication, context) -> {
+                    HttpServletRequest request = context.getRequest();
+                    HttpSession session = request.getSession(false);
+                    boolean hasAccess = session != null && session.getAttribute("authenticated") != null
+                            && (Boolean) session.getAttribute("authenticated");
+                    return new AuthorizationDecision(hasAccess);
+                })
             )
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
