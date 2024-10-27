f4fb85393fdcfedfe9a15cd2a3a0f3ef8228d4c7
AkashFestivs <gadhaveakash999@gmail.com>
2024-10-27 21:45:41 +0530

    commit

---
+++ src/test/java/com/ama/karate/services/ClassesServiceTest.java
@@ -0,0 +1,35 @@
+package com.ama.karate.services;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ama.karate.dto.ClassesDto;
+import com.ama.karate.service.ClassesService;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class ClassesServiceTest {
+
+    @Autowired
+    private ClassesService service;
+
+    @Test
+    void testBringUserClasses_ReturnsExpectedNumberOfClasses() {
+        String phoneNo = "2345678901";
+
+        List<ClassesDto> result = service.bringUserClasses(phoneNo);
+
+        // Assert
+        assertNotNull(result, "The result should not be null.");
+        assertTrue(!result.isEmpty(), "Expected at least one class returned from the database.");
+        
+    }
+}
